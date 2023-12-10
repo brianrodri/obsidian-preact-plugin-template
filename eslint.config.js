8f4a481b2ddaffe7e30efb19a2a9eec6d44b09f5
Brian Rodriguez <brian@brianrodri.com>
2023-12-10 14:15:15 -0500

    style: install prettier and format files

--- eslint.config.js
+++ eslint.config.js
@@ -1,35 +1,45 @@
 import js from "@eslint/js";
+import prettierConfig from "eslint-config-prettier";
+import prettierPlugin from "eslint-plugin-prettier";
 import typescriptPlugin from "@typescript-eslint/eslint-plugin";
 import typescriptPluginParser from "@typescript-eslint/parser";
 import globals from "globals";
 
 /** @type { import("eslint").Linter.FlatConfig[] } */
 export default [
-	{ ignores: ["dist/", "node_modules/"] },
+    { ignores: ["dist/", "node_modules/"] },
 
-	js.configs.recommended,
+    js.configs.recommended,
 
-	{
-		files: ["version-bump.mjs", "{eslint,vite}.config.js"],
-		languageOptions: {
-			globals: { ...globals.node },
-		},
-	},
+    {
+        files: ["version-bump.mjs", "{eslint,vite}.config.js"],
+        languageOptions: {
+            globals: { ...globals.node },
+        },
+    },
 
-	{
-		files: ["src/**/*.ts"],
-		plugins: { "@typescript-eslint": typescriptPlugin },
-		rules: {
-			...typescriptPlugin.configs.recommended.rules,
-			...typescriptPlugin.configs.strict.rules,
-		},
-		languageOptions: {
-			globals: { ...globals.browser },
-			parser: typescriptPluginParser,
-			parserOptions: {
-				ecmaFeatures: { modules: true },
-				ecmaVersion: "latest",
-			},
-		},
-	},
+    {
+        files: ["src/**/*.ts"],
+        plugins: { "@typescript-eslint": typescriptPlugin },
+        rules: {
+            ...typescriptPlugin.configs.recommended.rules,
+            ...typescriptPlugin.configs.strict.rules,
+        },
+        languageOptions: {
+            globals: { ...globals.browser },
+            parser: typescriptPluginParser,
+            parserOptions: {
+                ecmaFeatures: { modules: true },
+                ecmaVersion: "latest",
+            },
+        },
+    },
+
+    {
+        plugins: { prettier: prettierPlugin },
+        rules: {
+            ...prettierConfig.rules,
+            "prettier/prettier": "error",
+        },
+    },
 ];
