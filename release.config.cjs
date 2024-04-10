27aa7a4294a04cf30bf3be06d5114fde65607d25
Brian Rodriguez <brian@brianrodri.com>
2024-04-10 07:52:05 -0400

    build: commit version changes as part of release

--- release.config.cjs
+++ release.config.cjs
@@ -6,6 +6,13 @@ module.exports = {
         "@semantic-release/release-notes-generator",
         "brianrodri/semantic-release-obsidian-plugin",
         [
+            "@semantic-release/git",
+            {
+                assets: ["package.json", "package-lock.json", "manifest.json", "versions.json"],
+                message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+            },
+        ],
+        [
             "@semantic-release/github",
             {
                 assets: ["package.json", "package-lock.json", "manifest.json", "versions.json"],
